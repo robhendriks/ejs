c20da97ad9d01b9b2bc215da7609cba1e3c36d9d
robhendriks <cyanidenl@icloud.com>
2025-12-10 11:47:47 +0100

    fix: remove qjsx example code from main

--- src/ejs.cpp
+++ src/ejs.cpp
@@ -1,31 +1,10 @@
+#include <spdlog/spdlog.h>
+
 #include "app.h"
 
 int main(const int argc, const char **argv) {
     ejs::App app{ejs::AppOptions::getDefault()};
     app.init();
-
-    /*Runtime rt;
-    rt.addModulePath("plugins/*", R"(C:\Git\ejs\plugins\*\main.js)");
-
-    const Context ctx{rt};
-
-    const Value example = ctx.evalModule("plugins/foo");
-    if (example.isException()) {
-        example.printException();
-    }
-
-    const Value exampleModule = ctx.getModule("plugins/foo");
-    if (exampleModule.isObject()) {
-        const Value result = exampleModule["main"]();
-
-        if (result.isException()) {
-            result.printException();
-        } else {
-            spdlog::info("Result: {}", result.as<String>().data());
-        }
-    }
-
-    spdlog::info("done");*/
 
     return 0;
 }
